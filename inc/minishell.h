a4f16785458cfacbf7937c8adffee96aacd58b1d
Insar Assambekov <iassambe@car15s5.42barcelona.com>
2023-12-09 15:57:27 +0100

    Add Makefile, src, libft, readline, inc

---
+++ inc/minishell.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iassambe <iassambe@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 15:49:09 by iassambe          #+#    #+#             */
+/*   Updated: 2023/12/09 15:49:25 by iassambe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/time.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <dirent.h>
+# include <termios.h>
+# include <term.h>
+# include <curses.h>
+# include <signal.h>
+//# include <readline/readline.h>
+//# include <readline/history.h>
+
+#endif
