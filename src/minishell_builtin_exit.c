4ef30a804495a05f01b44ebb87690d97bf4c4542
InsAsam <108653442+InsAsam@users.noreply.github.com>
2024-01-16 13:47:59 +0100

    Starting export and exit builtins

--- src/minishell_builtin_exit.c
+++ src/minishell_builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: iassambe <iassambe@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 04:05:26 by iassambe          #+#    #+#             */
-/*   Updated: 2024/01/15 17:19:26 by iassambe         ###   ########.fr       */
+/*   Updated: 2024/01/16 13:29:37 by iassambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 int	builtin_exit(t_msh *msh)
 {
-	(void)(msh);
+	if (msh->exec.exec_arg[1])
+	{
+		
+	}
 	return (0);
 }
--- src/minishell_builtin_export.c
+++ src/minishell_builtin_export.c
@@ -6,14 +6,26 @@
 /*   By: iassambe <iassambe@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 04:03:50 by iassambe          #+#    #+#             */
-/*   Updated: 2024/01/15 17:19:28 by iassambe         ###   ########.fr       */
+/*   Updated: 2024/01/16 13:47:35 by iassambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+void	builtin_export_print_all(t_msh *msh)
+{
+	int	i;
+
+	i = -1;
+	while (msh->ev[++i])
+	{
+		
+	}
+}
+
 int	builtin_export(t_msh *msh)
 {
-	(void)(msh);
+	if (!msh->exec.exec_arg[1])
+		builtin_export_print_all(msh);
 	return (0);
 }
