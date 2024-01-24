2825e3e2c686d98702031af1a30fd1a4adf680aa
Insar Assambekov <iassambe@car15s3.42barcelona.com>
2024-01-24 19:45:31 +0100

    Fixing executing

--- src/minishell_builtin_unset.c
+++ src/minishell_builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: iassambe <iassambe@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 04:05:05 by iassambe          #+#    #+#             */
-/*   Updated: 2024/01/24 17:45:49 by iassambe         ###   ########.fr       */
+/*   Updated: 2024/01/24 19:12:53 by iassambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,36 @@ void	unset_env_line(t_msh *msh, int line_num)
 	msh->ev = new_env;
 }
 
+int	unset_check_var(char **exec_arg)
+{
+	int	i;
+	int	status;
+
+	status = 0;
+	i = 1;
+	while (exec_arg[i])
+	{
+		if (check_ifempty_str(exec_arg[i]) || \
+			ft_strchr(exec_arg[i], '=') || ft_isdigit(exec_arg[i][0]))
+		{
+			print_warning_with_3_arg("unset", exec_arg[i], ERR_INVALID_INDENT);
+			status = 1;
+		}
+		i++;
+	}
+	return (status);
+}
+
 int	builtin_unset(t_msh *msh)
 {
 	char	*var;
 	int		i;
 	int		line_num;
 
+	if (unset_check_var(msh->exec.exec_arg))
+	{
+		return (1);
+	}
 	i = 0;
 	while (msh->exec.exec_arg[++i])
 	{
